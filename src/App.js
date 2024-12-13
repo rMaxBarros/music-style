d8214154b6c3ea9f32c438d7335aabb13422b61c
rMaxBarros <rmaxbarros@gmail.com>
2024-12-13 15:30:36 -0300

    Add css for App and Header styles

--- src/App.js
+++ src/App.js
@@ -2,13 +2,17 @@ import Form from "./components/Form";
 import Header from "./components/Header";
 import Terms from "./components/Terms";
 
+import './App.css';
+
 function App() {
   return (
     <>
-      <Header />
-      <h2>Inscreva-se e tenha atualizações sobre shows perto de você!</h2>
-      <Form />
-      <Terms />
+      <main>
+        <Header />
+        <h2>Inscreva-se e tenha atualizações sobre shows perto de você!</h2>
+        <Form />
+        <Terms />
+      </main>
     </>
   );
 }
