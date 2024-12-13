817c1a1a91157de3d594250fb5dd81b1b8696019
rMaxBarros <rmaxbarros@gmail.com>
2024-12-13 14:27:02 -0300

    add header icon and title

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -3,7 +3,13 @@ import './Header.css';
 function Header () {
     return (
         <>
-
+            <header>
+                <img src="./images/logo-music-style.svg" alt="logo music style" />
+                <div>
+                    <h1>Music Style</h1>
+                    <img></img>
+                </div>
+            </header>
         </>
     )
 }
