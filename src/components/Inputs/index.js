e25352ee3dbc5bba0ccc1456525e3350ee283010
rMaxBarros <rmaxbarros@gmail.com>
2024-12-13 15:56:30 -0300

    Rename props in Inputs

--- src/components/Inputs/index.js
+++ src/components/Inputs/index.js
@@ -3,7 +3,7 @@ import './Inputs.css';
 function Inputs (props) {
     return (
         <>
-            <input className={props.whichBorder} type={props.type} placeholder={props.placeholder}></input>
+            <input className={props.whichInput} type={props.type} placeholder={props.placeholder}></input>
         </>
     )
 }
