8d61258277db7c516bfc44481e62faa7ad29358d
rMaxBarros <rmaxbarros@gmail.com>
2024-12-13 15:13:21 -0300

    add Terms component and h2 title in App

---
+++ src/components/Terms/index.js
@@ -0,0 +1,12 @@
+function Terms () {
+    return (
+        <>
+            <div className="terms">
+                <input type="checkbox" id="terms-accepted"></input>
+                <p>Ao inscrever-se, você concorda em receber e-mails informativos do Music Style.</p>
+            </div>
+        </>
+    )
+}
+
+export default Terms
